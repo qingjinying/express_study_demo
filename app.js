8ffaf0df11c00bddf1d4170c66db348983210657
qingjinying <932496603@qq.com>
2018-09-05 17:58:50 +0800

    start express
    simple demo

---
+++ app.js
@@ -0,0 +1,29 @@
+const CONFIG_PORT = 3000;
+
+let express = require('express');
+
+let app = express();
+
+app.get('/lol',function (req, res, next) {
+    res.send('欢迎来到英雄联盟');
+
+});
+
+app.get('/error', function (req, res, next) {
+    next('出错了');
+});
+
+app.use(function (err, req, res, next) {
+    console.log(err);
+    res.send('ERROR');
+});
+
+app.use(function (req, res, next) {
+    console.log('404 NOT FOUND');
+    res.send('404')
+});
+
+app.listen(CONFIG_PORT, function () {
+    console.log('hello world');
+});
+
